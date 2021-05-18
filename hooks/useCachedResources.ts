15d46d877cad1cdb52ffc3e01eae4638f48eed37
smlee <smlee@motionlabs.kr>
2021-05-18 22:45:39 +0900

    styles: 이사만루 폰트 추가

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -16,6 +16,8 @@ export default function useCachedResources() {
         await Font.loadAsync({
           ...Ionicons.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'esamanru-medium': require('../assets/fonts/esamanru-Medium.ttf'),
+          'esamanru-light': require('../assets/fonts/esamanru-Light.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
