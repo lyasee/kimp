a1cae3c72039ad6e021eba92e2fbd99c72ea86c2
smlee <smlee@motionlabs.kr>
2021-05-20 22:00:35 +0900

    styles: 배경색 통일

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -60,130 +60,139 @@ export default function TabTwoScreen() {
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('TradingViewDominance');
-        }}>
-        <View style={styles.boxWrapper}>
-          <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
-            <Text style={styles.boxTitle}>도미넌스</Text>
-            <View
-              style={{ ...styles.dominanceBox, backgroundColor: colors.bitcoinBox, marginTop: 8 }}>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('TradingViewDominance');
+          }}>
+          <View style={styles.boxWrapper}>
+            <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
+              <Text style={styles.boxTitle}>도미넌스</Text>
               <View
                 style={{
-                  ...styles.dominanceTextWrapper,
+                  ...styles.dominanceBox,
                   backgroundColor: colors.bitcoinBox,
+                  marginTop: 8,
                 }}>
                 <View
-                  style={{ ...styles.dominanceTextWrapper, backgroundColor: colors.bitcoinBox }}>
-                  <Text style={styles.dominanceLabel}>BTC</Text>
-                  <Text style={styles.dominancePercentage}>
-                    {dominance.btc ? dominance.btc.toString().trim() : '0'}%
-                  </Text>
+                  style={{
+                    ...styles.dominanceTextWrapper,
+                    backgroundColor: colors.bitcoinBox,
+                  }}>
+                  <View
+                    style={{ ...styles.dominanceTextWrapper, backgroundColor: colors.bitcoinBox }}>
+                    <Text style={styles.dominanceLabel}>BTC</Text>
+                    <Text style={styles.dominancePercentage}>
+                      {dominance.btc ? dominance.btc.toString().trim() : '0'}%
+                    </Text>
+                  </View>
+
+                  <View
+                    style={{ ...styles.dominanceTextWrapper, backgroundColor: colors.bitcoinBox }}>
+                    <Text style={styles.dominanceLabel}>, ETH</Text>
+                    <Text style={styles.dominancePercentage}>
+                      {dominance.eth ? dominance.eth.toString().trim() : '0'}%
+                    </Text>
+                  </View>
                 </View>
 
-                <View
-                  style={{ ...styles.dominanceTextWrapper, backgroundColor: colors.bitcoinBox }}>
-                  <Text style={styles.dominanceLabel}>, ETH</Text>
-                  <Text style={styles.dominancePercentage}>
-                    {dominance.eth ? dominance.eth.toString().trim() : '0'}%
-                  </Text>
+                <View style={{ backgroundColor: colors.bitcoinBox }}>
+                  <Text style={styles.dominanceLabel}>차트보기</Text>
                 </View>
-              </View>
-
-              <View style={{ backgroundColor: colors.bitcoinBox }}>
-                <Text style={styles.dominanceLabel}>차트보기</Text>
               </View>
             </View>
           </View>
-        </View>
-      </TouchableOpacity>
+        </TouchableOpacity>
 
-      <View style={styles.boxWrapper}>
-        <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
-          <Text style={styles.boxTitle}>바이낸스</Text>
-          <View style={{ ...styles.priceWrapper, backgroundColor: colors.bitcoinBox }}>
-            <Text style={styles.openPrice}>
-              open {binance.openPrice.toFixed(2).toLocaleString()}
-            </Text>
-            <Text
-              style={{
-                ...styles.rate,
-                color: getBitcoinPriceColor(binance.price, binance.openPrice),
-              }}>
-              {binance.price.toFixed(2).toLocaleString()}
-            </Text>
-            <Text
-              style={{
-                color: getBitcoinPriceColor(binance.price, binance.openPrice),
-              }}>
-              {getBitcoinPricePercentagMark(binance.price, binance.openPrice)}
-              {Math.abs(binance.priceChangePercent)}%
-            </Text>
-          </View>
-        </View>
-      </View>
-
-      {coins['KRW-BTC'] && (
         <View style={styles.boxWrapper}>
           <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
-            <Text style={styles.boxTitle}>업비트</Text>
+            <Text style={styles.boxTitle}>바이낸스</Text>
             <View style={{ ...styles.priceWrapper, backgroundColor: colors.bitcoinBox }}>
               <Text style={styles.openPrice}>
-                open {coins['KRW-BTC'].prev_closing_price.toLocaleString()}
+                open {binance.openPrice.toFixed(2).toLocaleString()}
               </Text>
               <Text
                 style={{
                   ...styles.rate,
-                  color: getBitcoinPriceColor(
-                    coins['KRW-BTC'].trade_price,
-                    coins['KRW-BTC'].prev_closing_price,
-                  ),
+                  color: getBitcoinPriceColor(binance.price, binance.openPrice),
                 }}>
-                {coins['KRW-BTC'].trade_price.toLocaleString()}
+                {binance.price.toFixed(2).toLocaleString()}
               </Text>
               <Text
                 style={{
-                  color: getBitcoinPriceColor(
-                    coins['KRW-BTC'].trade_price,
-                    coins['KRW-BTC'].prev_closing_price,
-                  ),
+                  color: getBitcoinPriceColor(binance.price, binance.openPrice),
                 }}>
-                {getBitcoinPricePercentagMark(
-                  coins['KRW-BTC'].trade_price,
-                  coins['KRW-BTC'].prev_closing_price,
-                )}
-                {(coins['KRW-BTC'].change_rate * 100).toFixed(2)}%
+                {getBitcoinPricePercentagMark(binance.price, binance.openPrice)}
+                {Math.abs(binance.priceChangePercent)}%
               </Text>
             </View>
           </View>
         </View>
-      )}
 
-      <View style={styles.boxWrapper}>
-        <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
-          <Text style={styles.boxTitle}>Long</Text>
-          <Text style={{ ...styles.rate, color: colors.priceRise }}>{rate.long}%</Text>
+        {coins['KRW-BTC'] && (
+          <View style={styles.boxWrapper}>
+            <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
+              <Text style={styles.boxTitle}>업비트</Text>
+              <View style={{ ...styles.priceWrapper, backgroundColor: colors.bitcoinBox }}>
+                <Text style={styles.openPrice}>
+                  open {coins['KRW-BTC'].prev_closing_price.toLocaleString()}
+                </Text>
+                <Text
+                  style={{
+                    ...styles.rate,
+                    color: getBitcoinPriceColor(
+                      coins['KRW-BTC'].trade_price,
+                      coins['KRW-BTC'].prev_closing_price,
+                    ),
+                  }}>
+                  {coins['KRW-BTC'].trade_price.toLocaleString()}
+                </Text>
+                <Text
+                  style={{
+                    color: getBitcoinPriceColor(
+                      coins['KRW-BTC'].trade_price,
+                      coins['KRW-BTC'].prev_closing_price,
+                    ),
+                  }}>
+                  {getBitcoinPricePercentagMark(
+                    coins['KRW-BTC'].trade_price,
+                    coins['KRW-BTC'].prev_closing_price,
+                  )}
+                  {(coins['KRW-BTC'].change_rate * 100).toFixed(2)}%
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+
+        <View style={styles.boxWrapper}>
+          <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
+            <Text style={styles.boxTitle}>Long</Text>
+            <Text style={{ ...styles.rate, color: colors.priceRise }}>{rate.long}%</Text>
+          </View>
         </View>
-      </View>
 
-      <View style={styles.boxWrapper}>
-        <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
-          <Text style={styles.boxTitle}>Short</Text>
-          <Text style={{ ...styles.rate, color: colors.priceFall }}>{rate.short}%</Text>
+        <View style={styles.boxWrapper}>
+          <View style={{ ...styles.box, backgroundColor: colors.bitcoinBox }}>
+            <Text style={styles.boxTitle}>Short</Text>
+            <Text style={{ ...styles.rate, color: colors.priceFall }}>{rate.short}%</Text>
+          </View>
         </View>
-      </View>
 
-      <View style={styles.footer}>
-        <Footer />
-      </View>
-    </ScrollView>
+        <View style={styles.footer}>
+          <Footer />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  scrollView: {
     paddingTop: 8,
     paddingBottom: 8,
   },
